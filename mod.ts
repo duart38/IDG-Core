92f83a84f4ae6e51ad2bf605a3536a9343af8f00
duart38 <mr.codercuracao@gmail.com>
2021-06-30 18:50:41 +0200

    updated example

--- mod.ts
+++ mod.ts
@@ -1,21 +1,37 @@
 import { decode, encode } from "https://deno.land/x/pngs/mod.ts";
+import Builder from "./ImageBuilder/Builder.ts";
+import IDGRuntime from "./ImageBuilder/Runtime.ts";
 import { ActionID } from "./interfaces/Actions.ts";
 import { FileShape } from "./interfaces/FileShape.ts";
 
 
-//const exampleFile: FileShape = JSON.parse(Deno.readTextFileSync("./example.json"));
-const exampleFile: FileShape = {
-    "imageMap": [255, 0, 0, 255, 0, 0, 0, 255],
-    "instructions": [
-        [ActionID.interval, 5000, [
-            [ActionID.modifyPixel, 0, [0, 255, 0, 255]]
-        ]]
-    ]
-}
-console.log(JSON.stringify(exampleFile));
-// [0,0,0,0] -> R,G,B,A (range: 0-255)
-const data = new Uint8Array(exampleFile.imageMap);
-// Encode the image to have width 2 and height 1 pixel
-const png = encode(data, 2, 1);
+// const exampleFile: FileShape = {
+//     width: 4, height: 4,
+//     "imageMap": [255, 0, 0, 255, 0, 0, 0, 255],
+//     "instructions": [
+//         [ActionID.interval, 5000, [
+//             [ActionID.modifyPixel, 0, [0, 255, 0, 255]]
+//         ]]
+//     ]
+// }
 
-await Deno.writeFile("image.png", png);
+const CelBuilder = new Builder(2,2);
+CelBuilder.addInstructions([
+    CelBuilder.modifyPixel(0, [0,0,0,1]),
+    CelBuilder.render(),
+    CelBuilder.atInterval(3000, [
+        CelBuilder.modifyPixel(0, [255,255,255,255]),
+        CelBuilder.render()
+    ])
+]);
+
+const x = new IDGRuntime(CelBuilder.IDG);
+x.start();
+
+
+// const data = new Uint8Array(exampleFile.imageMap);
+// const png = encode(data, 2, 1);
+
+// await Deno.writeFile("image.png", png);
+
+
