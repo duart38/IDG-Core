3eda007a5e31b17c5cdcfe5a5d9eac14eb952997
duart38 <mr.codercuracao@gmail.com>
2021-07-03 10:46:26 +0200

    added container for upcoming loader

---
+++ IDGVM/Loader.ts
@@ -0,0 +1,7 @@
+/**
+ * Should take in a .idg image and figure out based on the headers how allocation should be
+ * done.. when this is done we can call the CPU. think of this as a "usb port" of sorts.
+ * 
+ * i'm making this in a modular way because there could be a case where we load directly
+ * from for example a memory stick (exhibition mode) or from a network signal (event mode)
+ */
