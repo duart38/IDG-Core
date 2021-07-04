a0d394ef59fd2f1853e955456848f48d7b5adfb0
duart38 <mr.codercuracao@gmail.com>
2021-07-04 09:47:19 +0200

    adapted sample code to see the effects of 32-bit

--- IDGVM/mod.ts
+++ IDGVM/mod.ts
@@ -4,8 +4,8 @@ import { Instructions } from "./Registers.ts";
 
 const MM = new MemoryMapper();
 
-const memory = createMemory(256*256);
-MM.map(memory, 0, 0xffff);
+const memory = createMemory((256 * 256 * 256 * 256));
+MM.map(memory, 0, 0x7FFFFFFF);
 
 // Map 0xFF bytes of the address space to an "output device"
 //MM.map(CreateDumpToDiskDisplay() as unknown as any, 0x3000, 0x30ff, true); // TODO: figure out first param
@@ -13,23 +13,35 @@ MM.map(memory, 0, 0xffff);
 const writableBytes = new Uint8Array(memory.buffer);
 let i = 0;
 const cpu = new IDGVM(MM);
-
+// cpu.debug()
+const PADDING = 0x00;
 
 writableBytes[i++] = Instructions.MOV_LIT_REG
-writableBytes[i++] = 0x00
-writableBytes[i++] = 0x05
-writableBytes[i++] = 0x02
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = 5
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = 2
+cpu.step()
+cpu.debug()
 
+// TODO: not reading the next instruction
 writableBytes[i++] = Instructions.ADD_REG_REG
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
 writableBytes[i++] = 0x02
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
 writableBytes[i++] = 0x02
-
-cpu.debug()
 cpu.step()
 cpu.debug()
-cpu.step()
-cpu.debug()
 
+cpu.viewMemoryAt(0, 16);
 
 
 // cpu.run();
