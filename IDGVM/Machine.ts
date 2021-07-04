b2d409cfc091efdc1db94a6debb799acf348f36d
duart38 <mr.codercuracao@gmail.com>
2021-07-04 13:01:13 +0200

    XOR_REG_REG -> 32-bit

--- IDGVM/Machine.ts
+++ IDGVM/Machine.ts
@@ -490,15 +490,13 @@ export default class IDGVM {
         return;
       }
 
-      // Xor register with register
+      // Xor a registers value with another registers value and puts the results in the accumulator
       case Instructions.XOR_REG_REG: {
         const r1 = this.fetchRegisterIndex();
         const r2 = this.fetchRegisterIndex();
-        const registerValue1 = this.registers.getUint16(r1);
-        const registerValue2 = this.registers.getUint16(r2);
-
-        const res = registerValue1 ^ registerValue2;
-        this.setRegister('acc', res);
+        const registerValue1 = this.registers.getUint32(r1);
+        const registerValue2 = this.registers.getUint32(r2);
+        this.setRegister('acc', registerValue1 ^ registerValue2);
         return;
       }
 
