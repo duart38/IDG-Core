43b308860449019beaa660d563127ab9268c6a48
duart38 <mr.codercuracao@gmail.com>
2021-07-04 21:58:07 +0200

    just testing

--- IDGVM/mod.ts
+++ IDGVM/mod.ts
@@ -1,3 +1,4 @@
+import { CreateDumpToDiskDisplay } from "./Display.ts";
 import IDGVM from "./Machine.ts";
 import { createMemory, MemoryMapper } from "./Memory.ts";
 import { Instructions } from "./Registers.ts";
@@ -8,51 +9,16 @@ const memory = createMemory((256 * 256 * 256 * 256));
 MM.map(memory, 0, 0x7FFFFFFF);
 
 // Map 0xFF bytes of the address space to an "output device"
-//MM.map(CreateDumpToDiskDisplay() as unknown as any, 0x3000, 0x30ff, true); // TODO: figure out first param
+MM.map(CreateDumpToDiskDisplay() as unknown as any, 0xC8, 0xFF, true); // TODO: figure out first param
 
 const writableBytes = new Uint8Array(memory.buffer);
 let i = 0;
 const cpu = new IDGVM(MM);
 // cpu.debug()
 const PADDING = 0x00;
+cpu.debug();
 
 writableBytes[i++] = Instructions.MOV_LIT_REG
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = 0 // literal
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = 2 // register
-cpu.step()
-cpu.debug()
-
-writableBytes[i++] = Instructions.MOV_LIT_REG
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = 3 // literal
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = 3 // register
-cpu.step()
-
-writableBytes[i++] = Instructions.JMP_NOT_EQ
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = 5
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = PADDING
-writableBytes[i++] = 27
-cpu.step()
-cpu.debug()
-
-
-writableBytes[i++] = Instructions.ADD_LIT_REG // jump to here
 writableBytes[i++] = PADDING
 writableBytes[i++] = PADDING
 writableBytes[i++] = PADDING
@@ -60,15 +26,31 @@ writableBytes[i++] = 1
 writableBytes[i++] = PADDING
 writableBytes[i++] = PADDING
 writableBytes[i++] = PADDING
-writableBytes[i++] = 2
+writableBytes[i++] = 3
 
-cpu.step()
-cpu.step()
-cpu.step()
-cpu.step()
-cpu.step()
-cpu.debug()
+writableBytes[i++] = Instructions.MOV_REG_MEM
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = 3
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = PADDING
+writableBytes[i++] = 0xC8
 
+// writableBytes[i++] = Instructions.HLT
+
+
+cpu.viewMemoryAt(0x2BC, 16)
+
+
+let stepCount = 0;
+while(1){
+    alert(`####### STEP ####### ${stepCount}`);
+    cpu.step();
+    cpu.debug();
+    stepCount++;
+}
 
 
 // writableBytes[i++] = Instructions.ADD_REG_REG
