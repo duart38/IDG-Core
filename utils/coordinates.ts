f8b5091ae37805e429d11725bbd4b024f3b500b5
duart38 <mr.codercuracao@gmail.com>
2021-07-05 18:18:39 +0200

    extracted get neighb pixel method

--- utils/coordinates.ts
+++ utils/coordinates.ts
@@ -1,3 +1,5 @@
+import { Direction } from "../interfaces/Actions.ts";
+
 export function indexByCoordinates(x: number, y: number, w: number): number {
     return ((y * w) + x); // * 4;
 }
@@ -11,4 +13,44 @@ export function clamp(value: number, length: number): number{
     if(value > length) return length;
     if(value < 0) return 0;
     return value;
+}
+
+export function getNeighboringPixelIndex(direction: Direction, from: number, width: number) {
+    switch(direction){
+        case Direction.left: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x-1, y, width);
+        }
+        case Direction.right: {
+            // return from + 4, this.;
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x+1, y, width);
+        }
+        case Direction.topLeft: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x-1, y-1, width);
+        }
+        case Direction.top: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x, y-1, width);
+        }
+        case Direction.topRight: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x+1, y-1, width);
+        }
+        case Direction.bottomRight: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x+1, y+1, width);
+        }
+        case Direction.bottom: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x, y+1, width);
+        }
+        case Direction.bottomLeft: {
+            const [x,y] = coordinatesByIndex(from, width);
+            return indexByCoordinates(x-1, y+1, width);
+        }
+        default: console.log("Invalid directional instruction");
+    }
+    return 0;
 }
