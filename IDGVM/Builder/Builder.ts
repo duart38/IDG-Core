7fa097ff66e69075cefc54e807346de25442dd6c
duart38 <mr.codercuracao@gmail.com>
2021-07-05 18:39:48 +0200

    Added flag or label support

--- IDGVM/Builder/Builder.ts
+++ IDGVM/Builder/Builder.ts
@@ -5,11 +5,21 @@ import { ImageData } from "../../interfaces/Image.ts";
  * It also keeps track of the memory requirements based on what requests are made.
  */
 export default class IDGBuilder {
-    private instructionIndex = 0;
+    public instructionIndex = 0;
     private memoryRequirement = 0;
     private imageData: ImageData;
+    private flags: Record<string, number>;
     constructor(imageData: ImageData){
         this.imageData = imageData;
+        this.flags = {};
     }
-    //TODO: all instructions here... along with some other ones that group some instructions
+    //TODO: all instructions here... along with some other ones that group some instructions\
+
+    /**
+     * Sets a flag at the current instruction index so that you can refer back to it in code later.
+     * Very useful for jumping around as you don't need to remember the locations in memory.
+     */
+    setFlag(name: string){
+        this.flags[name] = this.instructionIndex
+    }
 }
