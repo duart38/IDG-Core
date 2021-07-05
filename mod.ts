0d17515ca2ca816cf8fd81ec263a3e29967da844
duart38 <mr.codercuracao@gmail.com>
2021-07-05 15:43:58 +0200

    just testing

--- mod.ts
+++ mod.ts
@@ -1,11 +1,24 @@
 import { coordinatesByIndex, indexByCoordinates } from "./utils/coordinates.ts";
+import { combineRGB } from "./utils/color.ts";
+import { createMemory } from "./IDGVM/Memory.ts";
 
-const test = [ // 5 x 3
-    0,1,2,3,0,
-    0,4,5,6,0,
-    0,7,8,9,0,
-]
-let res = indexByCoordinates(2, 1, 5);
-const [x,y] = coordinatesByIndex(res, 5);
 
-console.log(res, x, y)
+console.log(combineRGB([255,255,255])); // 16777215
+
+console.log(combineRGB([255,0,0])); // 16711680 (RED)
+
+
+
+// let result = chunkUp(16711680);
+// console.log("parts", result);
+
+// let mem = createMemory(4);
+// let wriatble = new Uint8Array(mem.buffer);
+// wriatble[0] = result[0];
+// wriatble[1] = result[1];
+// wriatble[2] = result[2];
+// wriatble[3] = result[3];
+
+// console.log(mem.getUint32(0));
+
+
