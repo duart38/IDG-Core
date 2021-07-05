4587df33635480d142cf63f8c92e711bb1d40fbf
duart38 <mr.codercuracao@gmail.com>
2021-07-05 13:31:09 +0200

    updated image display

--- IDGVM/IO/Display.ts
+++ IDGVM/IO/Display.ts
@@ -14,16 +14,14 @@
 //     console.log('\x1b[0m');
 //   }
 
-export const CreateDumpToDiskDisplay = () => {
+export const CreateImageDisplay = () => {
   return {
     getUint16: () => 0,
     getUint8: () => 0,
     setUint16: () => 0,
     // Triggered when something in the overlapping memory of this I/O is set
     setUint32: (address: number, data: number) => {
-      //const command = (data & 0xff00) >> 8;
-      //const characterValue = data & 0x00ff;
-      console.log("\n\n\n\n\n",address, data,"\n\n\n\n\n\n\n");
+      console.log(`Image data has been updated at address: ${address}, with data: ${data}`);
     },
   };
 };
