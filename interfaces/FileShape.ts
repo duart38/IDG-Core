73e7ac28dc19f07ffef0c71fcecd08da909f7db2
duart38 <mr.codercuracao@gmail.com>
2021-07-06 16:59:15 +0200

    removed unused code

--- interfaces/FileShape.ts
+++ interfaces/FileShape.ts
@@ -1,15 +1,3 @@
-import { instruction } from "./Actions.ts";
-
-/**
- * @deprecated
- */
-export interface FileShape {
-    width: number, height: number
-    imageMap: number[],
-    memory: Record<number, number>,
-    instructions: instruction[]
-}
-
 /**
  * [imageWidth, imageHeight, MemoryAllocation]
  */
