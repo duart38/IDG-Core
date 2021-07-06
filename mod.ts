5b4832aa42a428da30e6156660b20d8d43cf31d1
duart38 <mr.codercuracao@gmail.com>
2021-07-06 22:23:20 +0200

    testing new system

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import IDGBuilder from "./IDGVM/Builder/Builder.ts";
 import IDGLoader from "./IDGVM/Loader.ts";
-
+import { encode } from "https://deno.land/x/pngs/mod.ts";
 
 const builder = new IDGBuilder({
     imageData: [
@@ -14,11 +14,22 @@ const builder = new IDGBuilder({
     width: 10, height: 6
 });
 
-const compiled = builder.compile();
 
+
+builder.drawRectangle(5,5,  0,0, [255,255,255]);
+builder.RENDER();
+
+
+const compiled = builder.compile();
 
 
 
 // test loading compiled code
 const loader = new IDGLoader(compiled);
-//loader.
+loader.onImageUpdate((data)=>{
+    const png = encode(new Uint8Array(data), 10, 6);
+    Deno.writeFile("image.png", png).catch((x)=>{
+        console.log(x);
+    })
+})
+loader.startVM();
