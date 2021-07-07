80bddaee35cb1ae90b0eb4b6a24f7b9289f98d9e
duart38 <mr.codercuracao@gmail.com>
2021-07-07 10:28:11 +0200

    added helper compress and decompress methods

--- utils/bits.ts
+++ utils/bits.ts
@@ -1,4 +1,5 @@
 import { U255 } from "../interfaces/RGBA.ts";
+import { gunzip, gzip } from "https://deno.land/x/compress@v0.3.8/mod.ts";
 
 /**
  * Chunk up a 32-bit value into an array of 8 bit values
@@ -24,4 +25,11 @@ export function chunkUp32(num: number): [U255, U255, U255, U255]{
     parts[0] = ((num & 0x0000FF00)>>8) as U255;
     parts[1] = ((num & 0x000000FF)) as U255;
     return parts;
+}
+
+export function compress(rawData: Uint8Array){
+    return gzip(rawData);
+}
+export function deCompress(rawData: Uint8Array){
+    return gunzip(rawData)
 }
