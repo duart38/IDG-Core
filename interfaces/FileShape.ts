4410fd6045530d666b163c1a861eee4fe7fee00e
duart38 <mr.codercuracao@gmail.com>
2021-07-07 10:39:15 +0200

    added a shape that identifies how a decoded file could look like

--- interfaces/FileShape.ts
+++ interfaces/FileShape.ts
@@ -2,3 +2,10 @@
  * [imageWidth, imageHeight, MemoryAllocation]
  */
 export type IDGHeader = [number, number, number];
+
+export interface DecodedFile {
+    imageWidth: number, imageHeight: number,
+    memoryRequest: number,
+    image: number[],
+    memorySection: Uint8Array
+}
