493a6b2251158f8bdf2790365ddb1805939c5fe4
duart38 <mr.codercuracao@gmail.com>
2021-07-08 17:09:09 +0200

    added test images

--- mod.ts
+++ mod.ts
@@ -1,11 +1,23 @@
 import IDGBuilder from "./IDGVM/Builder/Builder.ts";
 import IDGLoader from "./IDGVM/Loader.ts";
 import { Instructions } from "./IDGVM/Registers.ts";
-import { encode } from "https://deno.land/x/pngs/mod.ts";
+import { encode, decode } from "https://deno.land/x/pngs/mod.ts";
+import { combineRGB } from "./utils/color.ts";
+import { U255 } from "./interfaces/RGBA.ts";
+
+const loadedImage = decode(Deno.readFileSync("./test_image.png"));
+const x = loadedImage.image;
+let image: number[] = [];
+for(let i = 0; i < loadedImage.image.length; i += 4){
+    const r = x[i] as U255;
+    const g = x[i+1] as U255;
+    const b = x[i+2] as U255;
+    image.push(combineRGB([r,g,b]));
+}
 
 const builder = new IDGBuilder({
-    imageData: new Array(30*30).fill(9),
-    width: 30, height: 30
+    imageData: image,
+    width: loadedImage.width, height: loadedImage.height
 });
 
 // const testFunction = builder.functionBuilder();
@@ -33,7 +45,7 @@ const compiled = builder.compile();
 const loader = new IDGLoader(compiled);
 loader.onImageUpdate((data)=>{
     console.log("render called")
-    const png = encode(new Uint8Array(data), 30, 30);
+    const png = encode(new Uint8Array(data), loadedImage.width, loadedImage.height);
     Deno.writeFile("image.png", png).catch((x)=>{
         console.log("image encoding error: ", x);
     })
