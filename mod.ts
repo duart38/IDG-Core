dac8360a0b1b5cedaed2df005a5d72ede7227b95
duart38 <mr.codercuracao@gmail.com>
2021-07-08 14:52:27 +0200

    testing some stuff

--- mod.ts
+++ mod.ts
@@ -1,35 +1,52 @@
 import IDGBuilder from "./IDGVM/Builder/Builder.ts";
 import IDGLoader from "./IDGVM/Loader.ts";
-import { encode } from "https://deno.land/x/pngs/mod.ts";
 
 const builder = new IDGBuilder({
-    imageData: [
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-    ],
-    width: 10, height: 6
+    imageData: new Array(4*4).fill(9),
+    width: 4, height: 4
 });
 
+// builder.insertFunction("test", [Instructions.MODIFY_PIXEL, Instructions.RENDER])
+//     .modifyPixel()
+//     .RENDER()
+//     .return(false)
 
 
-builder.drawRectangle(5,5,  0,0, [255,255,255]);
-builder.RENDER();
+
+// builder.push("ip");
+// builder.callSkippedOrFunction("test");
+// const x = builder.StoreValueInMemory(6)
+// builder.MoveMemoryToRegister(x, "r2")
+
+const testFunction = builder.functionBuilder();
+testFunction.markStart()
+
+builder.RGBToColor([255,255,255])
+
+testFunction.markEnd();
+//testFunction.call(); // to call the function directly
+
+
+builder.modifyPixel();
+builder.StoreNumberToRegister(2, "r1");
+
+
+// TODO: this seems to make the INTERVAL instruction work. make it into an instruction and put it at the final position of ram
+// builder.setFlag("end");
+// builder.GOTO("end");
 
 
 const compiled = builder.compile();
 
-Deno.writeFile("./example.idg", compiled);
+// // Deno.writeFile("./example.idg", compiled);
 
-// test loading compiled code
-// const loader = new IDGLoader(compiled);
+// // test loading compiled code
+const loader = new IDGLoader(compiled);
 // loader.onImageUpdate((data)=>{
-//     const png = encode(new Uint8Array(data), 10, 6);
-//     Deno.writeFile("image.png", png).catch((x)=>{
-//         console.log(x);
-//     })
+//     console.log("render called")
+//     // const png = encode(new Uint8Array(data), 10, 6);
+//     // Deno.writeFile("image.png", png).catch((x)=>{
+//     //     console.log(x);
+//     // })
 // }, true, true);
-// loader.startVM();
+loader.startVM();
