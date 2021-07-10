ef5c4eaa1a0bcef855216f5ac3190b0b1e576cd4
duart38 <mr.codercuracao@gmail.com>
2021-07-10 12:23:43 +0200

    added helpers to draw lines

--- utils/coordinates.ts
+++ utils/coordinates.ts
@@ -1,3 +1,4 @@
+import IDGVM from "../IDGVM/Machine.ts";
 import { Direction } from "../interfaces/Actions.ts";
 
 export function indexByCoordinates(x: number, y: number, w: number): number {
@@ -54,4 +55,68 @@ export function getNeighboringPixelIndex(direction: Direction, from: number, wid
         default: console.log("Invalid directional instruction");
     }
     return 0;
+}
+
+function slopeOf(x1: number, y1: number, x2: number, y2: number){
+    return (y2-y1) / (y2-x1);
+}
+
+function plotLineLow(_this: IDGVM, x0: number, y0: number, x1: number, y1:number){
+    const dx = x1 - x0
+    let dy = y1 - y0
+    let yi = 1
+    const color = _this.getRegister("COL");
+    if(dy < 0){
+        yi = -1
+        dy = -dy
+    }
+
+    let D = (2 * dy) - dx
+    let y = y0
+
+    for(let x = x0; x < x1 + 1; x++){
+        //plot(x, y)
+        _this.setPixelColor(indexByCoordinates(x,y,_this.image.width), color);
+        if(D > 0){
+            y = y + yi
+            D = D + (2 * (dy - dx))
+        }else{
+            D = D + 2*dy
+        }
+    }      
+}
+
+function plotLineHigh(_this: IDGVM, x0: number, y0: number, x1: number, y1:number){
+    const color = _this.getRegister("COL");
+    let dx = x1 - x0
+    const dy = y1 - y0
+    let xi = 1
+    if(dx < 0){
+        xi = -1
+        dx = -dx
+    }
+
+    let D = (2 * dx) - dy
+    let x = x0
+
+    for(let y = y0; y < y1 + 1; y++){ // for y from y0 to y1
+        _this.setPixelColor(indexByCoordinates(x,y,_this.image.width), color);
+        if(D > 0){
+            x = x + xi
+            D = D + (2 * (dx - dy))
+        }else{
+            D = D + 2*dx
+        }
+    }
+}
+
+
+function drawLine(_this: IDGVM, x0: number, y0: number, x1: number, y1:number){
+    if(Math.abs(y1 - y0) < Math.abs(x1 - x0)){ //if abs(y1 - y0) < abs(x1 - x0)
+        if(x0 > x1) plotLineLow(_this, x1, y1, x0, y0)
+        else plotLineLow(_this, x0, y0, x1, y1)
+    }else{
+        if (y0 > y1) plotLineHigh(_this, x1, y1, x0, y0)
+        else plotLineHigh(_this, x0, y0, x1, y1)
+    }
 }
