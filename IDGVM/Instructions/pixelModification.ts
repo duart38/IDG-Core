a480b996b608948406f093f2d01852d4f57d8f18
duart38 <mr.codercuracao@gmail.com>
2021-07-24 15:32:42 +0200

    added luminosity modification instruction

--- IDGVM/Instructions/pixelModification.ts
+++ IDGVM/Instructions/pixelModification.ts
@@ -1,3 +1,4 @@
+import { modifyLuminosity } from "../../utils/color.ts";
 import { indexByCoordinates } from "../../utils/coordinates.ts";
 import IDGVM from "../Machine.ts";
 
@@ -6,6 +7,15 @@ export enum PixelModificationType {
     MODIFY_PIXEL_LIT_LIT_LIT,
     MODIFY_PIXEL_MEM_MEM_MEM,
     // TODO: all the possible combinations of the above
+}
+export enum LuminosityModificationType {
+    MODIFY_PIXEL_LUMINOSITY_REG,
+    MODIFY_PIXEL_LUMINOSITY_MEM,
+    MODIFY_PIXEL_LUMINOSITY_LIT,
+
+    MODIFY_IMAGE_LUMINOSITY_REG,
+    MODIFY_IMAGE_LUMINOSITY_MEM,
+    MODIFY_IMAGE_LUMINOSITY_LIT,
 }
 
 export function modifyPixel(_this: IDGVM, params: number[]){
@@ -32,6 +42,50 @@ export function modifyPixel(_this: IDGVM, params: number[]){
             const color = _this.getMemoryAt(params[4]); // third mem
             const index = indexByCoordinates(x,y, _this.image.width);
             _this.setPixelColor(index, color);
+            break;
+        }
+    }
+}
+
+export function modifyLuminosityIns(_this: IDGVM, params: number[]){
+    switch(params[1]){
+        case LuminosityModificationType.MODIFY_PIXEL_LUMINOSITY_REG: {
+            const luminosity = _this.getRegisterAt(params[2]);
+            const x = _this.getRegister("x");
+            const y = _this.getRegister("y");
+            const index = indexByCoordinates(x,y, _this.image.width);
+            _this.setPixelColor(index, modifyLuminosity(luminosity, _this.imageCopy[index]));
+            break;
+        }
+        case LuminosityModificationType.MODIFY_PIXEL_LUMINOSITY_MEM: {
+            const luminosity = _this.getMemoryAt(params[2]);
+            const x = _this.getRegister("x");
+            const y = _this.getRegister("y");
+            const index = indexByCoordinates(x,y, _this.image.width);
+            _this.setPixelColor(index, modifyLuminosity(luminosity, _this.imageCopy[index]));
+            break;
+        }
+        case LuminosityModificationType.MODIFY_PIXEL_LUMINOSITY_LIT: {
+            const luminosity = params[2];
+            const x = _this.getRegister("x");
+            const y = _this.getRegister("y");
+            const index = indexByCoordinates(x,y, _this.image.width);
+            _this.setPixelColor(index, modifyLuminosity(luminosity, _this.imageCopy[index]));
+            break;
+        }
+        case LuminosityModificationType.MODIFY_IMAGE_LUMINOSITY_REG: {
+            const luminosity = _this.getRegisterAt(params[2]);
+            for(let i = 0; i < _this.imageCopy.length; i++) _this.setPixelColor(i, modifyLuminosity(luminosity, _this.imageCopy[i]));
+            break;
+        }
+        case LuminosityModificationType.MODIFY_IMAGE_LUMINOSITY_MEM: {
+            const luminosity = _this.getMemoryAt(params[2]);
+            for(let i = 0; i < _this.imageCopy.length; i++) _this.setPixelColor(i, modifyLuminosity(luminosity, _this.imageCopy[i]));
+            break;
+        }
+        case LuminosityModificationType.MODIFY_IMAGE_LUMINOSITY_LIT: {
+            const luminosity = params[2];
+            for(let i = 0; i < _this.imageCopy.length; i++) _this.setPixelColor(i, modifyLuminosity(luminosity, _this.imageCopy[i]));
             break;
         }
     }
