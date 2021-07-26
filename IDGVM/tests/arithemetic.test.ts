4605bd2982b6b303ef1cb1c4b00b8e01540c4991
duart38 <mr.codercuracao@gmail.com>
2021-07-26 20:25:59 +0200

    add ADD_LIT_MEM adjusted all tests to be async to adhere to vm instruction execution

--- IDGVM/tests/arithemetic.test.ts
+++ IDGVM/tests/arithemetic.test.ts
@@ -10,38 +10,50 @@ import { Instructions } from "../Registers.ts";
 function makeBuilder(): Builder {
     return new Builder({width: 1, height: 1, imageData: [0]}, 100000);
 }
-function makeLoader(builder: Builder, autoStart = false){
+async function makeLoader(builder: Builder, autoStart = false){
     const loader = new IDGLoader(builder.compile());
     // Some pre-population.
     loader.getVM().setRegister("r1", 1);
     loader.getVM().setRegister("r2", 2);
     loader.getVM().setRegister("r3", 3);
-    if(autoStart) loader.startVM();
+    if(autoStart) await loader.startVM();
     return loader;
 }
-Deno.test("ADD_REG_REG", function (): void {
+Deno.test("ADD_REG_REG", async function () {
     const b = makeBuilder();
     b.insert8(Instructions.ADD);
     b.insert8(additionType.ADD_REG_REG);
     b.insert32(b._regKeyToIndex("r1"));
     b.insert32(b._regKeyToIndex("r2"));
-    assertEquals(makeLoader(b, true).getVM().getRegister("acc"), 3);
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 3);
 });
 
-Deno.test("ADD_LIT_REG", function (): void {
+Deno.test("ADD_LIT_REG", async function () {
     const b = makeBuilder();
     b.insert8(Instructions.ADD);
     b.insert8(additionType.ADD_LIT_REG);
     b.insert32(5);
     b.insert32(b._regKeyToIndex("r1"));
-    assertEquals(makeLoader(b, true).getVM().getRegister("acc"), 6);
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 6);
 });
 
-Deno.test("ADD_REG_LIT", function (): void {
+Deno.test("ADD_REG_LIT", async function () {
     const b = makeBuilder();
     b.insert8(Instructions.ADD);
     b.insert8(additionType.ADD_REG_LIT);
     b.insert32(b._regKeyToIndex("r1"));
     b.insert32(5);
-    assertEquals(makeLoader(b, true).getVM().getRegister("acc"), 6);
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 6);
+});
+
+Deno.test("ADD_LIT_MEM", async function () {
+    const b = makeBuilder();
+    const storeAt = b.instructionIndex + 30;
+    b.MoveRegisterToMemory("r1", storeAt);
+    b.insert8(Instructions.ADD);
+    b.insert8(additionType.ADD_LIT_MEM);
+    b.insert32(5);
+    b.insert32(storeAt);
+    const vm = (await makeLoader(b, true)).getVM();
+    assertEquals(vm.getRegister("acc"), 6);
 });
