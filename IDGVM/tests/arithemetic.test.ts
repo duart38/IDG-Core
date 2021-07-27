126a3f5ba42fd939129f8e2eb64bc11a60261c7d
duart38 <mr.codercuracao@gmail.com>
2021-07-27 10:27:03 +0200

    SUB_LIT_REG tests

--- IDGVM/tests/arithemetic.test.ts
+++ IDGVM/tests/arithemetic.test.ts
@@ -78,3 +78,12 @@ Deno.test("SUB_REG_REG", async function () {
     b.insert32(b._regKeyToIndex("r1"));
     assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 2);
 });
+
+Deno.test("SUB_LIT_REG", async function () {
+    const b = makeBuilder();
+    b.insert8(Instructions.SUBTRACT);
+    b.insert8(subtractionType.SUB_LIT_REG);
+    b.insert32(3);
+    b.insert32(b._regKeyToIndex("r1"));
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 2);
+});
