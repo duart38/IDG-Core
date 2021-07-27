100fbad6b8fa442782b1e1f213fd89983554cc24
duart38 <mr.codercuracao@gmail.com>
2021-07-27 10:40:12 +0200

    testing MUL_REG_MEM

--- IDGVM/tests/arithemetic.test.ts
+++ IDGVM/tests/arithemetic.test.ts
@@ -182,3 +182,14 @@ Deno.test("MUL_LIT_MEM", async function () {
     b.insert32(storeAt);
     assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 6);
 });
+
+Deno.test("MUL_REG_MEM", async function () {
+    const b = makeBuilder();
+    const storeAt = b.instructionIndex + 30;
+    b.MoveRegisterToMemory("r3", storeAt);
+    b.insert8(Instructions.MULTIPLY);
+    b.insert8(multiplicationType.MUL_REG_MEM);
+    b.insert32(b._regKeyToIndex("r2"));
+    b.insert32(storeAt);
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 6);
+});
