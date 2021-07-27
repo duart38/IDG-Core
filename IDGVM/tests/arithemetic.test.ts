0dd8359bd2f88ce2e28fb3dd99a2052adef5d90e
duart38 <mr.codercuracao@gmail.com>
2021-07-27 10:28:54 +0200

    testing SUB_LIT_MEM

--- IDGVM/tests/arithemetic.test.ts
+++ IDGVM/tests/arithemetic.test.ts
@@ -96,3 +96,14 @@ Deno.test("SUB_REG_LIT", async function () {
     b.insert32(1);
     assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 2);
 });
+
+Deno.test("SUB_LIT_MEM", async function () {
+    const b = makeBuilder();
+    const storeAt = b.instructionIndex + 30;
+    b.MoveRegisterToMemory("r1", storeAt);
+    b.insert8(Instructions.SUBTRACT);
+    b.insert8(subtractionType.SUB_LIT_MEM);
+    b.insert32(3);
+    b.insert32(storeAt);
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 2);
+});
