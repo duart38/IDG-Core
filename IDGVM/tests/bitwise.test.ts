5bc3f818eb15a5af2e5414edb87f5d832f691c3a
duart38 <mr.codercuracao@gmail.com>
2021-07-28 12:26:50 +0200

    testing AND_REG_MEM

--- IDGVM/tests/bitwise.test.ts
+++ IDGVM/tests/bitwise.test.ts
@@ -139,3 +139,14 @@ Deno.test("AND_REG_REG", async function () {
     b.insert32(b._regKeyToIndex("r3"));
     assertEquals((await makeLoader(b,true)).getVM().getRegister("r2"), 2);
 });
+
+Deno.test("AND_REG_MEM", async function () {
+    const b = makeBuilder();
+    const storeAt = b.instructionIndex + 30;
+    b.MoveRegisterToMemory("r3", storeAt);
+    b.insert8(Instructions.BITWISE_AND);
+    b.insert8(andType.AND_REG_MEM);
+    b.insert32(b._regKeyToIndex("r2"));
+    b.insert32(storeAt);
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("r2"), 2);
+});
