66f9105b329d92af657a1d0c1d774e8bfd803321
duart38 <mr.codercuracao@gmail.com>
2021-07-28 16:00:12 +0200

    testing OR_MEM_REG

--- IDGVM/tests/bitwise.test.ts
+++ IDGVM/tests/bitwise.test.ts
@@ -234,3 +234,14 @@ Deno.test("OR_MEM_LIT", async function () {
     b.insert32(3);
     assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 3);
 });
+
+Deno.test("OR_MEM_REG", async function () {
+    const b = makeBuilder();
+    const storeAt = b.instructionIndex + 30;
+    b.MoveRegisterToMemory("r2", storeAt);
+    b.insert8(Instructions.BITWISE_OR);
+    b.insert8(orType.OR_MEM_REG);
+    b.insert32(storeAt);
+    b.insert32(b._regKeyToIndex("r3"));
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("acc"), 3);
+});
