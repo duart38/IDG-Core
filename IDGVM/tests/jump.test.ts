a24722e2629617d7c01e5060d78ca227f5203cb9
duart38 <mr.codercuracao@gmail.com>
2021-07-29 11:45:39 +0200

    testing JNE_LIT (false)

--- IDGVM/tests/jump.test.ts
+++ IDGVM/tests/jump.test.ts
@@ -45,6 +45,15 @@ Deno.test("JNE_LIT (true)", async function () {
     assertEquals((await makeLoader(b,true)).getVM().getRegister("r3"), 69);
 });
 
+Deno.test("JNE_LIT (false)", async function () {
+    const b = makeBuilder();
+    b.insert8(Instructions.JMP_ACC);
+    b.insert8(AccJumpType.JNE_LIT);
+    b.insert32(0); // lit
+    b.insert32(b.getFlag("call")); // addr to jump to
+    assertNotEquals((await makeLoader(b,true)).getVM().getRegister("r3"), 69);
+});
+
 // Deno.test("IMAGE_WIDTH_REG", async function () {
 //     const b = makeBuilder();
 
