8578322d722c936ebc912ae2a82d8106b42d9528
duart38 <mr.codercuracao@gmail.com>
2021-07-29 10:09:43 +0200

    testing IMAGE_HEIGHT_REG

--- IDGVM/tests/imageInformation.test.ts
+++ IDGVM/tests/imageInformation.test.ts
@@ -8,7 +8,7 @@ import { Instructions } from "../Registers.ts";
  * Just a little to build a basic builder to ensure we instructions don't collide.
  **/
 function makeBuilder(): Builder {
-    return new Builder({width: 1, height: 1, imageData: [69]}, 100000);
+    return new Builder({width: 1, height: 2, imageData: [69, 20]}, 100000);
 }
 async function makeLoader(builder: Builder, autoStart = false){
     const loader = new IDGLoader(builder.compile());
@@ -28,7 +28,6 @@ Deno.test("IMAGE_WIDTH_REG", async function () {
     assertEquals((await makeLoader(b,true)).getVM().getRegister("r1"), 1);
 });
 
-
 Deno.test("IMAGE_WIDTH_MEM", async function () {
     const b = makeBuilder();
     const memStorage = b.instructionIndex + 40;
@@ -38,3 +37,11 @@ Deno.test("IMAGE_WIDTH_MEM", async function () {
     assertEquals((await makeLoader(b,true)).getVM().getMemoryAt(memStorage), 1);
 });
 
+Deno.test("IMAGE_HEIGHT_REG", async function () {
+    const b = makeBuilder();
+    const memStorage = b.instructionIndex + 40;
+    b.insert8(Instructions.FETCH_IMAGE_INFO);
+    b.insert8(ImageInfoFetchType.IMAGE_HEIGHT_REG);
+    b.insert32(b._regKeyToIndex("r1"));
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("r1"), 2);
+});
