257be4b5ce0f06e6f83eed6ddc9832b5c1d8f16f
duart38 <mr.codercuracao@gmail.com>
2021-07-29 11:40:35 +0200

    testing JNE_LIT

--- IDGVM/tests/jump.test.ts
+++ IDGVM/tests/jump.test.ts
@@ -11,11 +11,13 @@ import { Instructions } from "../Registers.ts";
 function makeBuilder(): Builder {
     let b = new Builder({width: 1, height: 2, imageData: [69, 20]}, 100000);
     // skip over a modification and jump to it for testing
-    b.skipInstructions("skipped", [Instructions.MOVE]);
+    b.skipInstructions("skipped", [Instructions.MOVE, Instructions.RET_TO_NEXT]);
+    b.setFlag("call");
     b.insert8(Instructions.MOVE);
     b.insert8(moveType.MOV_LIT_REG);
     b.insert32(69);
     b.insert32(b._regKeyToIndex("r3"));
+    b.insert8(Instructions.RET_TO_NEXT);
 
     return b;
 }
@@ -24,7 +26,7 @@ async function makeLoader(builder: Builder, autoStart = false){
     // Some pre-population.
     loader.getVM().setRegister("r1", 1);
     loader.getVM().setRegister("r2", 2);
-    loader.getVM().setRegister("r3", 0); // update this
+    loader.getVM().setRegister("r3", 3); // update this
     if(autoStart) await loader.startVM();
     return loader;
 }
@@ -34,10 +36,17 @@ Deno.test("Testing SKIP (jump depends on this)", async function () {
     assertNotEquals((await makeLoader(b,true)).getVM().getRegister("r3"), 69);
 });
 
+Deno.test("JNE_LIT (true)", async function () {
+    const b = makeBuilder();
+    b.insert8(Instructions.JMP_ACC);
+    b.insert8(AccJumpType.JNE_LIT);
+    b.insert32(1); // lit
+    b.insert32(b.getFlag("call")); // addr to jump to
+    assertEquals((await makeLoader(b,true)).getVM().getRegister("r3"), 69);
+});
+
 // Deno.test("IMAGE_WIDTH_REG", async function () {
 //     const b = makeBuilder();
-
-
 
 //     b.insert8(Instructions.JMP_ACC);
 //     b.insert8(AccJumpType.JNE_LIT);
