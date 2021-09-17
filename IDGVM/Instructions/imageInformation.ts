22c9ead85a28314ec40a039002ff28fc4105920e
duart38 <mr.codercuracao@gmail.com>
2021-09-17 08:57:58 -0400

    switch -> array lookup for image inf instructions

--- IDGVM/Instructions/imageInformation.ts
+++ IDGVM/Instructions/imageInformation.ts
@@ -10,36 +10,36 @@ export enum ImageInfoFetchType {
 }
 
 export function fetchImageInfo(_this: IDGVM, params: number[]) {
-  switch (params[1]) {
-    case ImageInfoFetchType.IMAGE_WIDTH_REG: {
+  [
+    /** case ImageInfoFetchType.IMAGE_WIDTH_REG: */
+      ()=> {
       const regToStoreIn = params[2];
       _this.setRegisterAt(regToStoreIn, _this.image.width);
-      break;
-    }
-    case ImageInfoFetchType.IMAGE_WIDTH_MEM: {
+    },
+    /** case ImageInfoFetchType.IMAGE_WIDTH_MEM: */
+      ()=> {
       const memToStoreIn = params[2];
       _this.setMemoryAt(memToStoreIn, _this.image.width);
-      break;
-    }
-    case ImageInfoFetchType.IMAGE_HEIGHT_REG: {
+    },
+    /** case ImageInfoFetchType.IMAGE_HEIGHT_REG: */
+      ()=> {
       const regToStoreIn = params[2];
       _this.setRegisterAt(regToStoreIn, _this.image.height);
-      break;
-    }
-    case ImageInfoFetchType.IMAGE_HEIGHT_MEM: {
+    },
+    /** case ImageInfoFetchType.IMAGE_HEIGHT_MEM: */
+      ()=> {
       const memToStoreIn = params[2];
       _this.setMemoryAt(memToStoreIn, _this.image.height);
-      break;
-    }
-    case ImageInfoFetchType.IMAGE_TOTAL_PIXELS_REG: {
+    },
+    /** case ImageInfoFetchType.IMAGE_TOTAL_PIXELS_REG: */
+      ()=> {
       const regToStoreIn = params[2];
       _this.setRegisterAt(regToStoreIn, _this.image.width * _this.image.height);
-      break;
-    }
-    case ImageInfoFetchType.IMAGE_TOTAL_PIXELS_MEM: {
+    },
+    /** case ImageInfoFetchType.IMAGE_TOTAL_PIXELS_MEM: */
+      ()=> {
       const memToStoreIn = params[2];
       _this.setMemoryAt(memToStoreIn, _this.image.width * _this.image.height);
-      break;
-    }
-  }
+    },
+  ][params[1]]();
 }
