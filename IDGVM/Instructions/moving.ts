1726f18e41cbe71f499b83b37a39c4b6ca7b1008
duart38 <mr.codercuracao@gmail.com>
2021-09-17 09:01:51 -0400

    switch -> arr lookup on moveType (non signed moves)

--- IDGVM/Instructions/moving.ts
+++ IDGVM/Instructions/moving.ts
@@ -19,61 +19,52 @@ export enum SMoveType {
  * @param param [instruction, moveType, param, param]
  */
 export function executeMove(_this: IDGVM, param: number[]) {
-  switch (param[1]) {
-    // Move literal value into register
-    case moveType.MOV_LIT_REG: {
+  [
+    /** Move literal value into register moveType.MOV_LIT_REG: */
+      ()=> {
       const literal = param[2];
       const register = param[3];
       _this.setRegisterAt(register, literal);
-      break;
-    }
+    },
 
-    // Move a registers value to another registers value
-
-    case moveType.MOV_REG_REG: {
+    /** Move a registers value to another registers value moveType.MOV_REG_REG: */
+      ()=> {
       const registerFrom = param[2];
       const registerTo = param[3];
       const value = _this.getRegisterAt(registerFrom);
       _this.setRegisterAt(registerTo, value);
-      break;
-    }
+    },
 
-    // Move a registers value to a location in memory
-
-    case moveType.MOV_REG_MEM: {
+    /** Move a registers value to a location in memory moveType.MOV_REG_MEM: */
+      ()=> {
       const registerFrom = param[2];
       const address = param[3];
       const value = _this.getRegisterAt(registerFrom);
       _this.setMemoryAt(address, value);
-      break;
-    }
+    },
 
-    // Move the value of a memory location to a register
-
-    case moveType.MOV_MEM_REG: {
+    /** Move the value of a memory location to a register moveType.MOV_MEM_REG: */
+      ()=> {
       const address = param[2];
       const registerTo = param[3];
       const value = _this.getMemoryAt(address);
       _this.setRegisterAt(registerTo, value);
-      break;
-    }
+    },
 
-    // Move a literal value to a memory location
-
-    case moveType.MOV_LIT_MEM: {
+    /** Move a literal value to a memory location moveType.MOV_LIT_MEM: */
+      ()=> {
       const value = param[2];
       const address = param[3];
       _this.setMemoryAt(address, value);
-      break;
-    }
-    case moveType.MOV_MEM_MEM: {
+   },
+    /** case moveType.MOV_MEM_MEM: */
+      ()=> {
       const addressFrom = param[2];
       const addressTo = param[3];
       const value = _this.getMemoryAt(addressFrom);
       _this.setMemoryAt(addressTo, value);
-      break;
-    }
-  }
+    },
+  ][param[1]]();
 }
 
 // TODO: will this even work? literals are signed but registers and mems are unsigned can we mix and match them?
